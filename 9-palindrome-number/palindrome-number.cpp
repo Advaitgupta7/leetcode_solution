1e870de4de7575d0b5512a015540d08163cd4bde
Advaitgupta7 <152624345+Advaitgupta7@users.noreply.github.com>
2026-01-27 15:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (24.77%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long long original = x;
+        long long r = 0;
+        while(x>0){
+            r = r*10+(x%10);
+            x = x/10;
+        }
+        return original == r;
+    }
+};
