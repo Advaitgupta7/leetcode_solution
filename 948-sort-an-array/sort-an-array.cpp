0c82be7d6a6480c0e0e379ad4153104161c6ee69
Advaitgupta7 <152624345+Advaitgupta7@users.noreply.github.com>
2026-02-08 23:48:20 +0530

    Time: 24 ms (83.75%) | Memory: 71.1 MB (72.01%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    void quickSort(vector<int>&nums, int low, int high){
-        if(low>=high) return;
-        int pivot = nums[low];
-        int i = low;
-        int j = high;
-        while(i<j){
-            while (i < j && nums[j] >= pivot) j--;
-            while (i < j && nums[i] <= pivot) i++;
-            swap(nums[i], nums[j]);
-        }
-        swap(nums[low], nums[i]);
-        quickSort(nums, low, i - 1);
-        quickSort(nums, i + 1, high);
-    }
     vector<int> sortArray(vector<int>& nums) {
-        quickSort(nums, 0, nums.size() - 1);
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
