3d60cf24f2429391be49e1525b377f56280ef5af
Advaitgupta7 <152624345+Advaitgupta7@users.noreply.github.com>
2026-05-08 01:29:17 +0530

    Time: 37 ms (80.28%) | Memory: 22.2 MB (48.98%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        
+        // Sort by length
+        sort(words.begin(), words.end(),
+            [](string &a, string &b) {
+                return a.size() < b.size();
+            });
+
+        unordered_map<string, int> dp;
+
+        int ans = 1;
+
+        for(string word : words) {
+
+            dp[word] = 1;
+
+            // Remove one character
+            for(int i = 0; i < word.size(); i++) {
+
+                string prev = word.substr(0, i) + word.substr(i + 1);
+
+                if(dp.count(prev)) {
+
+                    dp[word] = max(dp[word], dp[prev] + 1);
+                }
+            }
+
+            ans = max(ans, dp[word]);
+        }
+
+        return ans;
+    }
+};
