cb66929dbd6dd01df711dcbceb0b6eecdcf725e0
Advaitgupta7 <152624345+Advaitgupta7@users.noreply.github.com>
2026-06-24 16:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left=0;
+        int left = 0;
+        int right = nums.size()-1;
         for(int i=0;i<nums.size();i++){
-            if(nums[i] !=0){
-                nums[left]=nums[i];
+            if(nums[i]!=0){
+                nums[left] = nums[i];
                 left++;
             }
         }
-        for(int i=left;i<nums.size();i++) nums[i]=0;
+        for(int i = left;i<nums.size();i++){
+            nums[left] =0;
+            left++;
+        }
     }
 };
