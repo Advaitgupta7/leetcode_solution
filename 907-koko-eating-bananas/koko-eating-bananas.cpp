cd81c34181ddd6e5268769a753a47faa48b16a98
Advaitgupta7 <152624345+Advaitgupta7@users.noreply.github.com>
2026-06-29 16:39:36 +0530

    Time: 7 ms (70.80%) | Memory: 23 MB (13.16%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-
-    int findMax(vector<int>& piles){
-        int maxi = 0;
-        for(int i = 0;i<piles.size();i++){
-            maxi = max(maxi, piles[i]);
-        }
-        return maxi;
-    }
-
-    long long findTime(vector<int>& piles, int k){
-        long long time = 0;
-        for(int i = 0; i< piles.size(); i++){
-            time += ceil(double(piles[i]) / k);
-        }
-        return time;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = findMax(piles);
-        int l = 1;
-        int r = maxi;
-        int ans = 0;
-
-        while(l<=r){
-            int mid = (l+r)/2;
-            long long time = findTime(piles, mid);
-            if(time <= h){
-                r = mid - 1;
-                ans = mid;
+        int low = 1;
+        int high = *max_element(piles.begin(),piles.end());
+        while(low<high){
+            int mid = low +(high-low)/2;
+            long long hours =0;
+            for(int pile :piles){
+                hours += (pile+mid-1)/mid;
+            }
+            if(hours<=h){
+                high = mid;
             }
             else{
-                l = mid + 1;
+                low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
